2aeb300542b85d5ff644d869a8af058ec173bacc
123GAURAVTHADANI <garv.thadani@gmail.com>
2024-10-13 21:00:59 +0530

    Masterclass

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,4 +1,12 @@
-function Card() {
-  return <h4>Card</h4>;
+function Card({ item }) {
+  return (
+    <div className="card__container">
+      <h3>{item.title}</h3>
+      <img className="card__img" src={item.image} />
+      <div>
+        <button>Add to Cart</button>
+      </div>
+    </div>
+  );
 }
 export default Card;
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,24 @@
 import Card from "../Card/Card";
 import "../../index.css";
+import { useEffect, useState } from "react";
 
 function Main() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
+
   return (
     <div className="main__container">
       <h2>What’s Hot</h2>
+      <div className="main__section">
+        {products &&
+          products.map((item) => {
+            return <Card item={item} />;
+          })}
+      </div>
     </div>
   );
 }
